049999beddf3641d0f973e2ceba90582cf84e706
jiangtao.zuo <jiangtao.zuo@geely.com>
2022-03-09 15:45:39 +0800

    build: 添加less及less-loader依赖;

--- vue.config.js
+++ vue.config.js
@@ -86,6 +86,19 @@ const vueConfig = {
     }
   },
 
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          // less vars，customize ant design theme
+          'border-radius-base': '2px',
+        },
+        // DO NOT REMOVE THIS LINE
+        javascriptEnabled: true,
+      },
+    },
+  },
+
   devServer: {
     // development server port 8000
     port: 8000,
