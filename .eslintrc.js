efdd98bd31dc689cf5b5b2743f55bb7f07be05c1
jiangtao.zuo <jiangtao.zuo@geely.com>
2022-03-09 11:10:05 +0800

    fix: lint fix;

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/strongly-recommended', '@vue/standard'],
+  extends: ['plugin:vue/strongly-recommended', '@vue/standard', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: 'babel-eslint',
     ecmaFeatures: {
@@ -12,5 +12,31 @@ module.exports = {
   },
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0, // 禁止使用debugger
+    'prettier/prettier': [
+      2,
+      {
+        semi: false,
+      },
+      {
+        usePrettierrc: true,
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 5,
+        multiline: 1,
+      },
+    ],
+    'vue/attribute-hyphenation': 0,
+    'vue/html-self-closing': 0,
+    'vue/component-name-in-template-casing': 0,
+    'vue/html-closing-bracket-spacing': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/no-unused-components': 0,
+    'vue/multiline-html-element-content-newline': 0,
+    'vue/no-use-v-if-with-v-for': 0,
+    'vue/html-closing-bracket-newline': 0,
+    'vue/no-parsing-error': 0,
   },
 }
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
