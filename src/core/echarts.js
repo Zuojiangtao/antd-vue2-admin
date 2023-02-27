4e84862d530c253b9b348f99352771eb2014755f
ZuoJiangtao <1799158837@qq.com>
2023-02-27 11:17:38 +0800

    perf: 去除冗余echarts的子组件

--- src/core/echarts.js
+++ src/core/echarts.js
@@ -10,8 +10,6 @@ import {
   DatasetComponent,
   TransformComponent,
   LegendComponent,
-  MarkLineComponent,
-  MarkPointComponent,
 } from 'echarts/components'
 
 // 注意，新的接口中默认不再包含 Canvas 渲染器，需要显示引入，如果需要使用 SVG 渲染模式则使用 SVGRenderer
@@ -31,7 +29,5 @@ echarts.use([
   DatasetComponent,
   TransformComponent,
   LegendComponent,
-  MarkLineComponent,
-  MarkPointComponent,
   SVGRenderer,
 ])
